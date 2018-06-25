0dd10c991cce04b8f91a479b621104ba703086a6
Daniel Leong <daniel.leongsj@gmail.com>
2018-06-25 16:01:34 +0800

    add menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
-import { MatToolbarModule } from "@angular/material";
+import { MatToolbarModule, MatListModule } from "@angular/material";
 import { FlexLayoutModule } from "@angular/flex-layout";
 
 import "hammerjs";
 import { AppComponent } from "./app.component";
+import { MenuComponent } from "./menu/menu.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MenuComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
